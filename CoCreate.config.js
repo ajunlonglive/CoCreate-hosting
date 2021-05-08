ad134997bc1280c665fc493346ac574088c2274f
frankpagan <frank@cocreate.app>
2021-05-08 21:10:24 +0000

    fix: add css auto parse and save to docs.

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -1,37 +1,39 @@
 module.exports = {
-	"config": {
-		"apiKey": "c2b08663-06e3-440c-ef6f-13978b42883a",
-		"securityKey": "f26baf68-e3a9-45fc-effe-502e47116265",
-		"organization_Id": "5de0387b12e200ea63204d6c",
-		"host": "server.cocreate.app:8088"
-	},
-
-    "sources": [{
+    "config": {
+        "apiKey": "c2b08663-06e3-440c-ef6f-13978b42883a",
+        "organization_Id": "5de0387b12e200ea63204d6c",
+        "host": "server.cocreate.app:8088"
+    },
+    "sources": [
+        {
             "entry": "./docs/index.html",
             "collection": "files",
-            "document_id": "",
+            "document_id": "609025562ac2fe23051494a1",
             "key": "src",
-            "data":{
+            "data": {
                 "name": "hosting Doc",
-                "domains": ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
+                "domains": [
+                    "cocreate.app",
+                    "server.cocreate.app",
+                    "ws.cocreate.app"
+                ],
                 "path": "/docs/hosting"
             }
+        },
+        {
+            "entry": "./docs/images/logo.png",
+            "collection": "files",
+            "document_id": "609025562ac2fe23051494a2",
+            "key": "src",
+            "data": {
+                "name": "hosting Doc",
+                "domains": [
+                    "cocreate.app",
+                    "server.cocreate.app",
+                    "ws.cocreate.app"
+                ],
+                "path": "/docs/images/logo.png"
+            }
         }
-    ],
-
-	"extract": {
-		"directory": "./src/",
-		"extensions": [
-			"js",
-			"css",
-			"html"
-		],
-		"ignores": [
-			"node_modules",
-			"vendor",
-			"bower_components",
-			"archive"
-		]
-	}
+    ]
 }
-
